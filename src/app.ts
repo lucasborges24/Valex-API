775d68699ab4db48d99056e5d62d72ad213844a5
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-31 12:47:36 -0300

    chore: create app

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log("server running on PORT " + PORT);
+});
