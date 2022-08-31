5ed5080b8689d98b69c21647dafc798628e81bf1
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-31 13:02:49 -0300

    chore: add route layer

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import router from "./routes/index.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
