4a134ddb371c7b74a035e5e8c86bd2c1f691b9ea
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-31 15:48:45 -0300

    feat: add validators to card/:employeeId POST route

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -1,5 +1,21 @@
 import { Router, Request, Response } from "express";
 
+import { cardController } from "../controllers/index.js";
+import { validateParamsId } from "../middlewares/idMiddleware.js";
+import {
+  validateHeaderSchema,
+  validateBodySchema,
+} from "../middlewares/schemaMiddleware.js";
+import { apiKeySchema, typeSchema } from "../schemas/cardSchema.js";
+
 const cardRouter = Router();
+
+cardRouter.post(
+  "/card/:employeeId",
+  validateHeaderSchema(apiKeySchema),
+  validateBodySchema(typeSchema),
+  validateParamsId("employeeId"),
+  cardController.createCard
+);
 
 export { cardRouter };
