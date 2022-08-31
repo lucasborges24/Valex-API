d111c54fb5a01dc41cb4ab36d10559e3f6e91bab
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-31 13:03:09 -0300

    chore: export functions by all files

---
+++ src/repositories/index.ts
@@ -0,0 +1,6 @@
+export * as businessRepository from "./businessRepository.js";
+export * as cardRepository from "./cardRepository.js";
+export * as companyRepository from "./companyRepository.js";
+export * as employeeRepository from "./employeeRepository.js";
+export * as paymentRepository from "./paymentRepository.js";
+export * as rechargeRepository from "./rechargeRepository.js";
