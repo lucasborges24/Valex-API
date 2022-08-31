463cdb6e2911f13a977b00acbbd5fb2fd6b111d8
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-31 15:45:52 -0300

    feat: add errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import "express-async-errors";
 import cors from "cors";
 import dotenv from "dotenv";
 
 import router from "./routes/index.js";
+import errorHandler from "./middlewares/errorHandle.js";
 
 dotenv.config();
 
@@ -11,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use(errorHandler)
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
