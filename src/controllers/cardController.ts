497a4b64b42a543816f9e2f780903bd76c2befc5
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-31 16:16:55 -0300

    feat: check employer/company exists

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
+import { cardService } from "../services/index.js";
 
 export const createCard = async (req: Request, res: Response) => {
   const { apikey }: { apikey: string } = res.locals.headers;
   const { type }: { type: string } = res.locals.body;
-  const { id }: { id: number } = res.locals.id;
+  const employeeId: number = res.locals.id;
 
+  const company: object = await cardService.checkApiKeyBelongSomeCompany(
+    apikey
+  );
+  const employer: object = await cardService.checkEmployeeIdBelongsSomeEmployer(
+    employeeId
+  );
 
-  res.send("nada de erros");
+  res.send(employer);
 };
