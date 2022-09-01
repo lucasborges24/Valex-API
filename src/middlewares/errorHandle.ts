462f94537cec0354f386d098878b902eec749765
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-01 12:47:12 -0300

    feat: add error 409

--- src/middlewares/errorHandle.ts
+++ src/middlewares/errorHandle.ts
@@ -1,13 +1,20 @@
-import { Request, Response, NextFunction, ErrorRequestHandler } from "express"
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 
-const errorHandler: ErrorRequestHandler = async (error, req: Request, res: Response, next: NextFunction) => {   
-    if (error.type === 'Unprocessable_Entity') {
-        return res.status(422).send(error.message)
-    } else if (error.type === 'Not_Found') {
-        return res.status(404).send(error.message)
-    } 
+const errorHandler: ErrorRequestHandler = async (
+  error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error.type === "Unprocessable_Entity") {
+    return res.status(422).send(error.message);
+  } else if (error.type === "Not_Found") {
+    return res.status(404).send(error.message);
+  } else if (error.type === "Conflit") {
+    return res.status(409).send(error.message);
+  }
 
-    return res.status(500).send(error?.message)
-}
+  return res.status(500).send(error?.message);
+};
 
-export default errorHandler
+export default errorHandler;
