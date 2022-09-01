7350717dc8d3a06589e305729468a1d732162de4
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-01 12:51:36 -0300

    feat: add function to create a card

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,17 +1,41 @@
 import { Request, Response } from "express";
+import { faker } from "@faker-js/faker";
 import { cardService } from "../services/index";
+import {
+  CardInsertData,
+  TransactionTypes,
+} from "../repositories/cardRepository";
 
 export const createCard = async (req: Request, res: Response) => {
   const { apikey }: { apikey: string } = res.locals.headers;
-  const { type }: { type: string } = res.locals.body;
+  const { type }: { type: TransactionTypes } = res.locals.body;
   const employeeId: number = res.locals.id;
 
-  const company: object = await cardService.checkApiKeyBelongSomeCompany(
-    apikey
-  );
-  const employer: object = await cardService.checkEmployeeIdBelongsSomeEmployer(
+  const company = await cardService.checkApiKeyBelongSomeCompany(apikey);
+  const employer = await cardService.checkEmployeeIdBelongsSomeEmployer(
     employeeId
   );
+  await cardService.checkTypeCardByEmployee(type, employeeId);
 
-  res.send(employer);
+  const cardholderName: string = await cardService.obtainCardholderName(
+    employer.fullName
+  );
+  const expirationDate: string = await cardService.generateValidateCardDate();
+  const securityCode: string = await cardService.encryptCvc(
+    faker.random.numeric(3)
+  );
+  const newCard: CardInsertData = {
+    employeeId,
+    number: faker.random.numeric(12),
+    cardholderName,
+    securityCode,
+    expirationDate,
+    password: undefined,
+    isVirtual: false,
+    originalCardId: undefined,
+    isBlocked: false,
+    type,
+  };
+  await cardService.createCard(newCard);
+  res.status(201).send('Cartão criado!');
 };
