e5fe72d44398f2fcff4e6208b2018046606bf640
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-05 16:48:47 -0300

    feat: use purchase router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { cardRouter } from "./cardRouter";
+import { purchaseRouter } from "./purchaseRouter";
 import { rechargeRouter } from "./rechargeRouter";
 
 const router = Router();
 
 router.use(cardRouter);
-router.use(rechargeRouter)
+router.use(rechargeRouter);
+router.use(purchaseRouter);
 
 export default router;
